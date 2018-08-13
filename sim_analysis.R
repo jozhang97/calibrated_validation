01ac734d8f0c0f3db21f81c0e63af7fc28910e3d
JEFFREY ZHANG <jozhang@berkeley.edu>
2018-08-14 07:50:28 +1200

    smaller # of bins

--- sim_analysis.R
+++ sim_analysis.R
@@ -12,8 +12,8 @@ tip_analysis <- function (path) {
     tips = as.numeric(as.vector(rows))
 
     # TODO ask fabio to help with ggplot
-    hist(tips, nclass = 100, xlim = c(0, 1000), prob = TRUE)
-    hist(tips, nclass = 100, xlim = c(0, 100), prob = TRUE)
+    hist(tips, nclass = 10, xlim = c(0, 1000), prob = TRUE)
+    hist(tips, nclass = 10, xlim = c(0, 100), prob = TRUE)
 
     hist_path = paste0(path, "tips_hist_HI.png")
     print(hist_path)
