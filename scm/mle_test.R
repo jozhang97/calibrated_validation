cf42f8ca2209fe59e256299f316afa8494192c05
JEFFREY ZHANG <jozhang@berkeley.edu>
2018-10-12 15:23:09 +1300

    create beast string in simulation script

--- scm/mle_test.R
+++ scm/mle_test.R
@@ -1,10 +1,11 @@
 library(ape)
 library(diversitree)
-set.seed(1234)
+#set.seed(1234)
 
 args = commandArgs(TRUE)
 exp.name = args[1]
 dir = "div/"
+root = "/Users/jeff/Documents/Research/Phylogenetics/calibrated_validation/scm/"
 
 ## HELPERS
 # Compares the states in the truth and the reconstruction for given states/indices of interest
@@ -27,8 +28,14 @@ q = 1/20
 sim.time = 50
 # pars = c(lambda, lambda, mu, mu, q, q)
 
-pars = c(0.2, 0.4, 0.01, 0.1, 0.1, 0.4)  # from RevBayes exp
+#pars = c(0.5, 0.4, 0.02, 0.1, 0.1, 0.02)  # from RevBayes exp
+#pars = c(0.2, 0.4, 0.01, 0.1, 0.1, 0.4)
+#pars = c(0.04, 0.08, 0.01, 0.02, 0.04, 0.01)
+pars = c(0.08, 0.08, 0.01, 0.01, 0.01, 0.01)
+n.taxa = 22
+#n.taxa = 5
 names(pars) = c("l0", "l1", "m0", "m1", "q01", "q10")
+pars
 
 # Set up a handcrafted tree 
 #trstr = "((Human:2.0,Chimp:2.0)nd1:2.0,(Gorilla:2.0,Pizza:2.0)nd2:2.0)nd3:0.0;"
@@ -38,7 +45,7 @@ names(pars) = c("l0", "l1", "m0", "m1", "q01", "q10")
 #names(states) = phy$tip.label
 
 # Get a ground truth tree
-phy = tree.bisse(pars, max.taxa=22, x0=0)
+phy = tree.bisse(pars, max.taxa=n.taxa, x0=0)
 plot(phy)
 if (is.null(phy)) {
     print("bad tree")
@@ -49,16 +56,38 @@ print("Tree tips")
 print(tips)
 print("Tree in Newick format")
 write.tree(phy)
+tree.file.name = paste0(dir, exp.name, ".tree")
+write.tree(phy, file=tree.file.name)
 ntips = length(tips)
 node.truth = phy$node.state
 
 print("Node truths")
+
 print(node.truth)
+node.truth.save = node.truth
 node.truth.names = names(node.truth)
-node.truth = t(node.truth)
-node.truth = rbind(node.truth.names, node.truth)
+node.truth.save = t(node.truth.save)
+node.truth.save = rbind(node.truth.names, node.truth.save)
 node.file.name = paste0(dir, exp.name, "-node_truth.csv")
-write.table(node.truth, file=node.file.name, sep=",", col.names=FALSE, row.names=FALSE)
+write.table(node.truth.save, file=node.file.name, sep=",", col.names=FALSE, row.names=FALSE)
+
+
+tips.save = tips
+tips.save.names = names(tips.save)
+tips.save = t(tips.save)
+tips.save = rbind(tips.save.names, tips.save)
+tips.file.name = paste0(dir, exp.name, "-tips.csv")
+write.table(tips.save, file=tips.file.name, sep=",", col.names=FALSE, row.names=FALSE)
+
+
+beast.data = ""
+for (i in 1:length(tips)) {
+    beast.data = paste0(beast.data, names(tips[i]), "=", tips[i], ",")
+}   
+beast.data = substr(beast.data, 0, nchar(beast.data) - 1)
+beast.data.file.name = paste0(root, dir, exp.name, "-beast_str.txt")
+cat(beast.data, file=beast.data.file.name)
+
 
 # Calculate likelyhood on tree
 sampling.f = c(1,1)
